bc3759eb9a4a24c7a62f2c5b1efc0a41aa4424a5
Kundan Tripathi <kundantripathi1512@gmail.com>
2026-03-17 20:55:42 +0530

    pattern 1

--- Kundan-Tripathi/Patterns.java
+++ Kundan-Tripathi/Patterns.java
@@ -1,17 +1,25 @@
 import java.util.Scanner;
 
 public class Patterns {
+
+    static void print1(int num) {
+       for(int i = 0; i < num; i++){
+            for(int j = 0; j < num; j++){
+                System.out.print("* ");
+            }
+            System.out.println("");
+        } 
+    }
+
+    
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
 
         System.out.print("Enter the number:- ");
         int num = input.nextInt();
 
-        for(int i = 0; i < num; i++){
-            for(int j = 0; j < num; j++){
-                System.out.print("* ");
-            }
-            System.out.println("");
-        }
+        print1(num);
     }
+
+     
 }
